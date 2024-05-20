8f84ef8d030523ffc4d25de7084dafe641340dc0
xiohuang <xiohuang@amazon.com>
2024-05-20 17:32:22 +0800

    fix kda write iceberg issue

--- src/main/java/com/amazonaws/sample/FlinkCDCPorstgres.java
+++ src/main/java/com/amazonaws/sample/FlinkCDCPorstgres.java
@@ -8,92 +8,90 @@ import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-/**
- * Flink CDC 接入 Postgresql 的数据，写入Kafka
- */
+/** Flink CDC 接入 Postgresql 的数据，写入Kafka */
 public class FlinkCDCPorstgres {
 
-    public static void main(String[] args) {
-        //设置flink表环境变量
-        EnvironmentSettings fsSettings = EnvironmentSettings.newInstance()
-                .inStreamingMode()
-                .build();
+  public static void main(String[] args) {
+    // 设置flink表环境变量
+    EnvironmentSettings fsSettings = EnvironmentSettings.newInstance().inStreamingMode().build();
 
-        //获取flink流环境变量
-        StreamExecutionEnvironment exeEnv = StreamExecutionEnvironment.getExecutionEnvironment();
-        exeEnv.setParallelism(1);
+    // 获取flink流环境变量
+    StreamExecutionEnvironment exeEnv = StreamExecutionEnvironment.getExecutionEnvironment();
+    exeEnv.setParallelism(1);
 
-        //表执行环境
-        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(exeEnv, fsSettings);
+    // 表执行环境
+    StreamTableEnvironment tableEnv = StreamTableEnvironment.create(exeEnv, fsSettings);
 
-        Date date = new Date();
-        SimpleDateFormat dateFormat= new SimpleDateFormat("yyyyMMddhhmmss");
-        String curTime = dateFormat.format(date);
-        //拼接souceDLL
-        String sourceDDL =String.format(
-                "CREATE TABLE pgsql_source_portfolio (\n" +
-                        "        id INT,\n" +
-                        "        reward INT,\n" +
-                        "        channels STRING,\n" +
-                        "        difficulty STRING,\n" +
-                        "        duration STRING,\n" +
-                        "        offer_type STRING,\n" +
-                        "        offer_id STRING,\n" +
-                        "        modify_time STRING,\n" +
-                        "        create_time STRING\n" +
-                        ") WITH (\n" +
-                        " 'connector' = 'postgres-cdc',\n" +
-                        " 'hostname' = 'database-1.cghfgy0zyjlk.us-east-1.rds.amazonaws.com',\n" +
-                        " 'port' = '5432',\n" +
-                        " 'username' = 'postgres',\n" +
-                        " 'password' = 'nakYK2fV',\n" +
-                        " 'database-name' = 'norrisdb',\n" +
-                        " 'schema-name' = 'public',\n" +
-                        " 'decoding.plugin.name' = 'pgoutput',\n" +
-                        " 'debezium.slot.name' = 'flinksql_%s',\n" +
-                        " 'table-name' = 'portfolio'\n" +
-                        ")",curTime);
+    Date date = new Date();
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddhhmmss");
+    String curTime = dateFormat.format(date);
+    // 拼接souceDLL
+    String sourceDDL =
+        String.format(
+            "CREATE TABLE pgsql_source_portfolio (\n"
+                + "        id INT,\n"
+                + "        reward INT,\n"
+                + "        channels STRING,\n"
+                + "        difficulty STRING,\n"
+                + "        duration STRING,\n"
+                + "        offer_type STRING,\n"
+                + "        offer_id STRING,\n"
+                + "        modify_time STRING,\n"
+                + "        create_time STRING\n"
+                + ") WITH (\n"
+                + " 'connector' = 'postgres-cdc',\n"
+                + " 'hostname' = 'database-1.cghfgy0zyjlk.us-east-1.rds.amazonaws.com',\n"
+                + " 'port' = '5432',\n"
+                + " 'username' = 'postgres',\n"
+                + " 'password' = 'nakYK2fV',\n"
+                + " 'database-name' = 'norrisdb',\n"
+                + " 'schema-name' = 'public',\n"
+                + " 'decoding.plugin.name' = 'pgoutput',\n"
+                + " 'debezium.slot.name' = 'flinksql_%s',\n"
+                + " 'table-name' = 'portfolio'\n"
+                + ")",
+            curTime);
 
-        String strKafkaBostrap = "b-1.mskworkshopcluster.hpukmd.c1.kafka.us-east-1.amazonaws.com:9092,b-2.mskworkshopcluster.hpukmd.c1.kafka.us-east-1.amazonaws.com:9092,b-3.mskworkshopcluster.hpukmd.c1.kafka.us-east-1.amazonaws.com:9092";
-        String sinkKafkaDDL = String.format("CREATE TABLE kafka_pg_portfolio_json (\n" +
-                "        id INT,\n" +
-                "        reward INT,\n" +
-                "        channels STRING,\n" +
-                "        difficulty STRING,\n" +
-                "        duration STRING,\n" +
-                "        offer_type STRING,\n" +
-                "        offer_id STRING,\n" +
-                "        modify_time STRING,\n" +
-                "        create_time STRING,\n" +
-                "     PRIMARY KEY (id) NOT ENFORCED\n" +
-                ") with (\n" +
-                "    'connector' = 'kafka',\n" +
-                "    'topic' = 'pg_portfolio',\n" +
-                "    'properties.bootstrap.servers' = '%s',\n" +
-                "    'properties.group.id' = 'kafka_portfolio_pg_json_gid_001',\n" +
-//                "     'kafka.security.protocol' = 'SASL_SSL',\n"  +
-//                "     'kafka.sasl.mechanism' = 'software.amazon.msk.auth.iam.IAMLoginModule required;',\n"  +
-//                "     'kafka.sasl.client.callback.handler.class' = 'software.amazon.msk.auth.iam.IAMClientCallbackHandler',\n"  +
-//                " 'format' = 'changelog-json'\n" +
-                " 'format' = 'debezium-json',\n" +
-                " 'debezium-json.ignore-parse-errors'='true'\n" +
-                ")",strKafkaBostrap);
+    String strKafkaBostrap =
+        "b-1.mskworkshopcluster.hpukmd.c1.kafka.us-east-1.amazonaws.com:9092,b-2.mskworkshopcluster.hpukmd.c1.kafka.us-east-1.amazonaws.com:9092,b-3.mskworkshopcluster.hpukmd.c1.kafka.us-east-1.amazonaws.com:9092";
+    String sinkKafkaDDL =
+        String.format(
+            "CREATE TABLE kafka_pg_portfolio_json (\n"
+                + "        id INT,\n"
+                + "        reward INT,\n"
+                + "        channels STRING,\n"
+                + "        difficulty STRING,\n"
+                + "        duration STRING,\n"
+                + "        offer_type STRING,\n"
+                + "        offer_id STRING,\n"
+                + "        modify_time STRING,\n"
+                + "        create_time STRING,\n"
+                + "     PRIMARY KEY (id) NOT ENFORCED\n"
+                + ") with (\n"
+                + "    'connector' = 'kafka',\n"
+                + "    'topic' = 'pg_portfolio',\n"
+                + "    'properties.bootstrap.servers' = '%s',\n"
+                + "    'properties.group.id' = 'kafka_portfolio_pg_json_gid_001',\n"
+                +
+                //                "     'kafka.security.protocol' = 'SASL_SSL',\n"  +
+                //                "     'kafka.sasl.mechanism' =
+                // 'software.amazon.msk.auth.iam.IAMLoginModule required;',\n"  +
+                //                "     'kafka.sasl.client.callback.handler.class' =
+                // 'software.amazon.msk.auth.iam.IAMClientCallbackHandler',\n"  +
+                //                " 'format' = 'changelog-json'\n" +
+                " 'format' = 'debezium-json',\n"
+                + " 'debezium-json.ignore-parse-errors'='true'\n"
+                + ")",
+            strKafkaBostrap);
 
+    String transformSQL =
+        "INSERT INTO kafka_pg_portfolio_json " + "SELECT * " + "FROM pgsql_source_portfolio";
 
-
-        String transformSQL =
-                "INSERT INTO kafka_pg_portfolio_json " +
-                        "SELECT * " +
-                        "FROM pgsql_source_portfolio";
-
-        //执行source表ddl
-        tableEnv.executeSql(sourceDDL);
-        //执行sink表ddl
-        tableEnv.executeSql(sinkKafkaDDL);
-        //执行逻辑sql语句
-        TableResult tableResult = tableEnv.executeSql(transformSQL);
-
-    }
+    // 执行source表ddl
+    tableEnv.executeSql(sourceDDL);
+    // 执行sink表ddl
+    tableEnv.executeSql(sinkKafkaDDL);
+    // 执行逻辑sql语句
+    TableResult tableResult = tableEnv.executeSql(transformSQL);
+  }
 }
-
-
--- src/main/java/com/amazonaws/sample/KafkaToIceberg.java
+++ src/main/java/com/amazonaws/sample/KafkaToIceberg.java
@@ -25,6 +25,7 @@ import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.StatementSet;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,136 +33,171 @@ import org.slf4j.LoggerFactory;
 import java.util.Map;
 import java.util.Properties;
 
-/**
- * Iceberg Flink Streaming Job Run in KDA.
- *
- */
+/** Iceberg Flink Streaming Job Run in KDA. */
 public class KafkaToIceberg {
-	private static final Logger LOG = LoggerFactory.getLogger(KafkaToIceberg.class);
+  private static final Logger LOG = LoggerFactory.getLogger(KafkaToIceberg.class);
 
-	public static void main(String[] args) throws Exception {
-		// set up the streaming execution environment
+  public static void main(String[] args) throws Exception {
+    // set up the streaming execution environment
 
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-		final ParameterTool parameter = ParameterTool.fromArgs(args);
+    final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    final ParameterTool parameter = ParameterTool.fromArgs(args);
 
-		//read the parameters from the Kinesis Analytics environment
-		Map<String, Properties> applicationProperties = KinesisAnalyticsRuntime.getApplicationProperties();
-		Properties flinkProperties = null;
+    // read the parameters from the Kinesis Analytics environment
+    Map<String, Properties> applicationProperties =
+        KinesisAnalyticsRuntime.getApplicationProperties();
+    Properties flinkProperties = null;
 
-		String kafkaTopic = parameter.get("kafka-topic", "AWSKafkaTutorialTopic");
-		String brokers = parameter.get("brokers", "");
-//		String s3Path = parameter.get("s3Path", "");
-		String Warehouse = parameter.get("warehouse", "");
+    String kafkaTopic = parameter.get("kafka-topic", "AWSKafkaTutorialTopic");
+    String brokers = parameter.get("brokers", "");
+    //		String s3Path = parameter.get("s3Path", "");
+    String Warehouse = parameter.get("warehouse", "");
+    String ConsumGroup = parameter.get("consumgroup", "flink-workshop-group-01");
+    String sinkTableName = parameter.get("sinkTable", "aws_iceberg_sink_table");
 
-		if (applicationProperties != null) {
-			flinkProperties = applicationProperties.get("FlinkApplicationProperties");
-		}
+    if (applicationProperties != null) {
+      flinkProperties = applicationProperties.get("FlinkApplicationProperties");
+    }
 
-		if (flinkProperties != null) {
-			kafkaTopic = flinkProperties.get("kafka-topic").toString();
-			brokers = flinkProperties.get("brokers").toString();
-//			s3Path = flinkProperties.get("s3Path").toString();
-			Warehouse = flinkProperties.get("warehouse").toString();
-		}
+    if (flinkProperties != null) {
+      kafkaTopic = flinkProperties.get("kafka-topic").toString();
+      brokers = flinkProperties.get("brokers").toString();
+      //			s3Path = flinkProperties.get("s3Path").toString();
+      Warehouse = flinkProperties.get("warehouse").toString();
+      ConsumGroup = flinkProperties.get("consumgroup").toString();
+      sinkTableName = flinkProperties.get("sinkTable").toString();
+    }
 
-//		LOG.info("kafkaTopic is :" + kafkaTopic);
-//		LOG.info("brokers is :" + brokers);
-//		LOG.info("s3Path is :" + s3Path);
-//		LOG.info("hiveMetaStore is :" + hiveMetaStore);
+    // Process stream using sql API
+    CDCIcebergSqlExample.createAndDeployJob(env, Warehouse, brokers, kafkaTopic, ConsumGroup, sinkTableName);
+  }
+
+  public static class CDCIcebergSqlExample {
+
+    public static void createAndDeployJob(
+        StreamExecutionEnvironment env,
+        String warhehouse,
+        String kafkaBoostrapServer,
+        String topic,
+        String consumGroup,
+        String sinkTableName) {
+      StreamTableEnvironment streamTableEnvironment =
+          StreamTableEnvironment.create(env, EnvironmentSettings.newInstance().build());
+
+      Configuration configuration = streamTableEnvironment.getConfig().getConfiguration();
+      configuration.setString("execution.checkpointing.interval", "1 min");
+
+      final String icebergCatalog =
+          String.format(
+              "CREATE CATALOG glue_catalog WITH ( \n"
+                  + "'type'='iceberg', \n"
+                  + "'warehouse'='%s', \n"
+                  + "'catalog-impl'='org.apache.iceberg.aws.glue.GlueCatalog', \n"
+                  + "'io-impl'='org.apache.iceberg.aws.s3.S3FileIO');",
+              warhehouse);
+
+      LOG.info(icebergCatalog);
+      streamTableEnvironment.executeSql(icebergCatalog);
+
+      // kafka source
+      final String sourceSQLTopic1 =
+          String.format("CREATE TABLE kafka_source_table_01 (\n"
+                  + "                                    uuid STRING,\n"
+                  + "                                    user_name STRING,\n"
+                  + "                                    phone_number BIGINT,\n"
+                  + "                                    product_id INT,\n"
+                  + "                                    product_name STRING,\n"
+                  + "                                    product_type STRING,\n"
+                  + "                                    manufacturing_date INT,\n"
+                  + "                                    price FLOAT,\n"
+                  + "                                    unit INT,\n"
+                  + "                                    ts TIMESTAMP(3)\n"
+                  + ") with (\n"
+                  + "'connector' = 'kafka',\n"
+                  + "'topic' = '%s',\n"
+                  + "'properties.bootstrap.servers' = '%s',\n"
+                  + "'scan.startup.mode' = 'earliest-offset',\n"
+                  + "'properties.group.id' = '%s',\n"
+                  + "'json.timestamp-format.standard' = 'ISO-8601',\n"
+                  + "'format' = 'json'\n"
+                  + ");", "datafaker_user_order_list_01", kafkaBoostrapServer, consumGroup);
+      LOG.info(sourceSQLTopic1);
+      streamTableEnvironment.executeSql(sourceSQLTopic1);
+
+      final String sourceSQLTopic2 =
+              String.format(
+                      "CREATE TABLE kafka_source_table_02 (\n"
+                              + "                                    uuid STRING,\n"
+                              + "                                    user_name STRING,\n"
+                              + "                                    phone_number BIGINT,\n"
+                              + "                                    product_id INT,\n"
+                              + "                                    product_name STRING,\n"
+                              + "                                    product_type STRING,\n"
+                              + "                                    manufacturing_date INT,\n"
+                              + "                                    price FLOAT,\n"
+                              + "                                    unit INT,\n"
+                              + "                                    ts TIMESTAMP(3)\n"
+                              + ") with (\n"
+                              + "'connector' = 'kafka',\n"
+                              + "'topic' = '%s',\n"
+                              + "'properties.bootstrap.servers' = '%s',\n"
+                              + "'scan.startup.mode' = 'earliest-offset',\n"
+                              + "'properties.group.id' = '%s',\n"
+                              + "'json.timestamp-format.standard' = 'ISO-8601',\n"
+                              + "'format' = 'json'\n"
+                              + ");",
+                      "datafaker_user_order_list_02", kafkaBoostrapServer, consumGroup);
+
+      LOG.info(sourceSQLTopic2);
+      streamTableEnvironment.executeSql(sourceSQLTopic2);
+
+      //      String sinkTableName = "user_order_list_kafka_02";
+
+      final String IcebergSink =
+          String.format(
+              "CREATE TABLE IF NOT EXISTS glue_catalog.icebergdb.%s (\n"
+                  + "                                    uuid STRING,\n"
+                  + "                                    user_name STRING,\n"
+                  + "                                    phone_number BIGINT,\n"
+                  + "                                    product_id INT,\n"
+                  + "                                    product_name STRING,\n"
+                  + "                                    product_type STRING,\n"
+                  + "                                    manufacturing_date INT,\n"
+                  + "                                    price FLOAT,\n"
+                  + "                                    unit INT,\n"
+                  + "                                    ts TIMESTAMP_LTZ(3),"
+                  + "									 DT STRING\n"
+                  + ") PARTITIONED BY (DT) WITH (\n"
+                  + "'type'='iceberg',\n"
+                  + "'catalog-name'='glue_catalog',\n"
+                  + "'write.metadata.delete-after-commit.enabled'='true',\n"
+                  + "'write.metadata.previous-versions-max'='5',\n"
+                  + "'write.distribution-mode'='hash',\n"
+                  + "'format-version'='2');",
+              sinkTableName);
+      LOG.info(IcebergSink);
+
+      streamTableEnvironment.executeSql(IcebergSink);
+
+      StatementSet stmtSet = streamTableEnvironment.createStatementSet();
+
+      final String insertSql01 =
+              String.format("insert into glue_catalog.icebergdb.%s /*+ OPTIONS('write-parallelism'='4') */ \n"
+                      + "select *,DATE_FORMAT(ts, 'yyyyMMdd') AS DT from default_catalog.default_database.kafka_source_table_01;"
+                      , sinkTableName);
 //
-//		//Create Properties object for the Kafka consumer
-//		Properties kafkaProps = new Properties();
-//		kafkaProps.setProperty("bootstrap.servers", brokers);
+      stmtSet.addInsertSql(insertSql01.trim());
 
-		//Process stream using sql API
-		CDCIcebergSqlExample.createAndDeployJob(env, Warehouse, brokers, kafkaTopic);
-	}
-
-
-	public static class CDCIcebergSqlExample {
-
-		public static void createAndDeployJob(StreamExecutionEnvironment env,
-											  String warhehouse,
-											  String kafkaBoostrapServer,
-											  String topic)  {
-			StreamTableEnvironment streamTableEnvironment = StreamTableEnvironment.create(
-					env, EnvironmentSettings.newInstance().build());
+      final String insertSql02 =
+              String.format("insert into glue_catalog.icebergdb.%s \n"
+                      + "select *,DATE_FORMAT(ts, 'yyyyMMdd') AS DT from default_catalog.default_database.kafka_source_table_02;"
+                      , sinkTableName);
+//
+      stmtSet.addInsertSql(insertSql02.trim());
 
 
+      stmtSet.execute();
 
-			Configuration configuration = streamTableEnvironment.getConfig().getConfiguration();
-			configuration.setString("execution.checkpointing.interval", "1 min");
-
-
-			final String icebergCatalog = String.format("CREATE CATALOG glue_catalog WITH ( \n" +
-					"'type'='iceberg', \n" +
-					"'warehouse'='%s', \n" +
-					"'catalog-impl'='org.apache.iceberg.aws.glue.GlueCatalog', \n" +
-					"'io-impl'='org.apache.iceberg.aws.s3.S3FileIO');", warhehouse);
-
-//			final String icebergCatalog = String.format("CREATE CATALOG glue_catalog WITH ( \n" +
-//					"'type'='iceberg', \n" +
-//					"'warehouse'='%s', \n" +
-//					"'catalog-type'='glue', \n" +
-//					"'io-impl'='org.apache.iceberg.aws.s3.S3FileIO');", warhehouse);
-
-			LOG.info(icebergCatalog);
-			streamTableEnvironment.executeSql(icebergCatalog);
-
-			//kafka source
-			final String sourceSQL = String.format("CREATE TABLE kafka_source_table (\n" +
-					"                                    uuid STRING,\n" +
-					"                                    user_name STRING,\n" +
-					"                                    phone_number BIGINT,\n" +
-					"                                    product_id INT,\n" +
-					"                                    product_name STRING,\n" +
-					"                                    product_type STRING,\n" +
-					"                                    manufacturing_date INT,\n" +
-					"                                    price FLOAT,\n" +
-					"                                    unit INT,\n" +
-					"                                    ts TIMESTAMP(3)\n" +
-					") with (\n" +
-					"'connector' = 'kafka',\n" +
-					"'topic' = '%s',\n" +
-					"'properties.bootstrap.servers' = '%s',\n" +
-					"'scan.startup.mode' = 'earliest-offset',\n" +
-					"'properties.group.id' = 'flink-workshop-group-01',\n" +
-					"'json.timestamp-format.standard' = 'ISO-8601',\n" +
-					"'format' = 'json'\n" +
-					");", topic, kafkaBoostrapServer);
-
-			LOG.info(sourceSQL);
-			streamTableEnvironment.executeSql(sourceSQL);
-
-			final String IcebergSink = String.format("CREATE TABLE IF NOT EXISTS glue_catalog.icebergdb.user_order_list_kafka (\n" +
-					"                                    uuid STRING,\n" +
-					"                                    user_name STRING,\n" +
-					"                                    phone_number BIGINT,\n" +
-					"                                    product_id INT,\n" +
-					"                                    product_name STRING,\n" +
-					"                                    product_type STRING,\n" +
-					"                                    manufacturing_date INT,\n" +
-					"                                    price FLOAT,\n" +
-					"                                    unit INT,\n" +
-					"                                    ts TIMESTAMP_LTZ(3)\n" +
-					") WITH (\n" +
-					"'type'='iceberg',\n" +
-					"'catalog-name'='glue_catalog',\n" +
-					"'write.metadata.delete-after-commit.enabled'='true',\n" +
-					"'write.metadata.previous-versions-max'='5',\n" +
-					"'format-version'='2');");
-			LOG.info(IcebergSink);
-
-			streamTableEnvironment.executeSql(IcebergSink);
-
-			final String insertSql = "insert into glue_catalog.icebergdb.user_order_list_kafka \n" +
-					"select * from default_catalog.default_database.kafka_source_table;";
-			streamTableEnvironment.executeSql(insertSql);
-		}
-	}
-
+    }
+  }
 }
-
-
-
